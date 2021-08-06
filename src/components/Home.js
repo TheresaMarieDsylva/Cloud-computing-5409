a11d3f3ab2c0f67008e6aa1cf1d54899ec78bec4
Jeyanth Kishore Ramasamy <jeyanth.kishore@gmail.com>
2021-08-06 02:07:30 -0300

    Channel name add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { Button } from "bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import { ButtonGroup } from "react-bootstrap";
 import styled from "styled-components";
 import Navigation from "./Navigation";
@@ -7,6 +7,8 @@ import { Nav, Navbar, Form, FormControl, Container } from 'react-bootstrap';
 import '../stylesheets/home.css';
 import axios from "axios";
 
+
+let name = ""
 const GridWrapper = styled.div`
   display: inline;
   grid-gap: 10px;
@@ -18,21 +20,21 @@ const GridWrapper = styled.div`
 `;
 const handleSearch = (e) => {
   e.preventDefault()
+  name = (e.target.value)
   // console.log(setWord(e.target.value))
 }
 
 const Home = (props) => (
-
-  <GridWrapper>
+  < GridWrapper >
     {/* <Navigation /> */}
-    <br></br>
+    < br ></br>
     <div>
       <Form className="form-center">
         <FormControl type="text" placeholder="Search" style={{ position: "absolute", left: "20rem", top: "4rem", maxWidth: "400px" }} className="" onChange={handleSearch} />
       </Form>
       <button type="button" class="block" style={{ position: "absolute", left: "48rem", top: "3rem" }} onClick={() => {
         alert("Please wait till Processing")
-        axios.get("https://6d6pzlx7ll.execute-api.us-east-1.amazonaws.com/youtubeAPIHit/name=" + "BBC News").then(response => {
+        axios.get("https://6d6pzlx7ll.execute-api.us-east-1.amazonaws.com/youtubeAPIHit/name=" + name).then(response => {
           setTimeout(function () {
             alert("Click Dashboard to see your resul")
           }, 1000);
@@ -45,7 +47,7 @@ const Home = (props) => (
       >Dash Board</button>
     </div>
 
-  </GridWrapper>
+  </GridWrapper >
 );
 
 export default Home;
